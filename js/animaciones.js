5f923814c42906f4704b06193155c75a428f0a1c
Dario Valdez <dartecode@github.com>
2023-11-29 09:05:36 -0500

    V1.0

--- js/animaciones.js
+++ js/animaciones.js
@@ -18,3 +18,18 @@ const observer = new IntersectionObserver(triggerAnimation, options);
 animate.forEach(anima => {
     observer.observe(anima);
 });
+
+let animado2 = document.querySelectorAll(".animacion2");
+
+function mostrarScroll2() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (var i = 0; i < animado2.length; i++) {
+        let alturaAnimado2 = animado2[i].offsetTop;
+        if (alturaAnimado2 -400 < scrollTop) {
+            animado2[i].style.opacity = 1;
+            animado2[i].classList.add("mostrarDerecha")
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll2);
